950d1e799d6fbf1346a873144fa75d778f00ac7a
Savchek <32517761+Savchek@users.noreply.github.com>
2020-11-23 20:58:59 +0200

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+let textTagsWrap = document.getElementById('text-tags');
+
+init = () => {
+	textTagsWrap.querySelectorAll('input').forEach(e => {
+		e.addEventListener('change',);
+	});
+};
+
+init();
